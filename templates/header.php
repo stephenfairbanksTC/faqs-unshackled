39044d6e5f6452404d15a0b137538cfba5a4d443
Stephen Fairbanks <stephen.fairbanks@trueconnectivity.com>
2016-04-26 11:13:35 +0100

    Responsive Nav changes

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,23 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><span class="sr-only">UNSHACKLED.com</span><h3 class="">FAQs</h3></a>
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#faq-nav-collapse" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="brand navbar-brand" href="#"><span class="sr-only">UNSHACKLED.com</span><h3 class="">FAQs</h3></a>
+    </div>
 
-    <nav class="main-nav">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <nav class="main-nav collapse navbar-collapse" id="faq-nav-collapse">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
       endif;
       ?>
-    </nav>
-  </div>
+    </nav><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
 </header>
