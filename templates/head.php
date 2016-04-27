21754e12cd758d654f734e4e384f1acd5cfcbf8c
Stephen Fairbanks <stephen.fairbanks@trueconnectivity.com>
2016-04-27 17:22:28 +0100

    Style changes

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="<?php get_template_directory_uri(); ?>/dist/images/favicon.png">
   
   <script type="text/javascript" src="//fast.fonts.net/jsapi/4c372be3-2eff-4c76-b862-2adb49e6c97d.js"></script>
   <?php wp_head(); ?>
